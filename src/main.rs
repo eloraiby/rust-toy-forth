c94efd986ceca248159718de1b7fd542193072a9
Wael El Oraiby <wael.el-oraiby@adgear.com>
2018-02-19 16:58:57 -0500

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::fmt;
 use std::env;
 use std::collections::*;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 
 pub enum InputStream {
     File(io::BufReader<File>),
@@ -15,6 +15,13 @@ pub struct StreamReader {
     is      : InputStream,
     offset  : usize,        // where are we in the buffer
     buff    : Vec<char>,
+}
+
+pub enum EvalResult {
+    None,
+    StackUnderflow,
+    ReturnStackUnderflow,
+    WordNotFound(String),
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -29,17 +36,17 @@ pub enum OpCode {
 impl fmt::Display for OpCode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            OpCode::PushUSize(u) => write!(f, "PushUSize({})", u),
-            OpCode::Jmp(u) => write!(f, "Jmp({})", u),
-            OpCode::Call(u) => write!(f, "Call({})", u),
-            OpCode::Cond(u) => write!(f, "Cond({})", u),
-            OpCode::Ret => write!(f, "Ret"),
+            OpCode::PushUSize(u)    => write!(f, "PushUSize({})", u),
+            OpCode::Jmp(u)          => write!(f, "Jmp({})", u),
+            OpCode::Call(u)         => write!(f, "Call({})", u),
+            OpCode::Cond(u)         => write!(f, "Cond({})", u),
+            OpCode::Ret             => write!(f, "Ret"),
         }
     }
 }
 
 pub enum Word {
-    Native { is_macro: bool, f: fn(&mut VM) },
+    Native { is_macro: bool, f: fn(&mut VM) -> EvalResult },
     Interp { is_macro: bool, fip: usize },
 }
 
@@ -52,6 +59,8 @@ pub struct VM {
     i_2_w   : Vec<Word>,
     i_2_n   : Vec<String>,
     n_2_i   : HashMap<String, usize>,
+
+    except  : EvalResult,
 
     // REPL
     is_cm   : bool,                     // is it in compilation
@@ -87,7 +96,11 @@ impl StreamReader {
             let (res, is_eos) =
                 match self.is {
                     InputStream::File(ref mut f)    => (f.read_line(&mut buff), buff.len() == 0 ),
-                    InputStream::Cli(ref mut c)     => (c.read_line(&mut buff), false) 
+                    InputStream::Cli(ref mut c)     => {
+                        print!("> ");
+                        io::stdout().flush().expect("stdout couldn't flush");
+                        (c.read_line(&mut buff), false) 
+                    }
                 };
 
             match (res, is_eos)  {
@@ -131,57 +144,103 @@ mod core {
     use std::io;
     use std::io::Write;
 
+    struct NativeEntry {
+        name    : &'static str,
+        is_macro: bool,
+        f       : fn(&mut ::VM) -> ::EvalResult
+    }
+
     pub const WID_CONSUME : usize = 0;
 
-    fn define_word(vm: &mut ::VM) {
+    fn define_word(vm: &mut ::VM) -> ::EvalResult {
         let token   = vm.read_token();
         vm.is_cm    = true;
         let wid     = vm.i_2_w.len();
         vm.i_2_w.push(::Word::Interp { is_macro: false, fip: vm.code.len() });
         vm.i_2_n.push(token.clone());
         vm.n_2_i.insert(token, wid);
+        ::EvalResult::None
     }
 
-    fn define_immediate(vm: &mut ::VM) {
+    fn define_immediate(vm: &mut ::VM) -> ::EvalResult {
         let token   = vm.read_token();
         vm.is_cm    = false;
         let wid     = vm.i_2_w.len();
         vm.i_2_w.push(::Word::Interp { is_macro: true, fip: vm.code.len() });
         vm.i_2_n.push(token.clone());
         vm.n_2_i.insert(token, wid);
+        ::EvalResult::None
     }
 
-    fn ret(vm: &mut ::VM) {
+    fn ret(vm: &mut ::VM) -> ::EvalResult {
         let ip = vm.ret.pop().unwrap();
         vm.ip = ip;
+        ::EvalResult::None
     }
 
-    fn print_value_stack(vm: &mut ::VM) {
-        let stack : &Vec<usize> = &vm.stack;
+    fn print_value_stack(vm: &mut ::VM) -> ::EvalResult {
+        let stack = &vm.stack;
+        print!("[ ");
         for v in stack {
-            print!("{}", v);
-            io::stdout().flush().expect("stdout failed");
+            print!("{} ", v);
         }
+        print!("]\n");
+        io::stdout().flush().expect("stdout failed");
+        ::EvalResult::None
     }
 
-    fn print_return_stack(vm: &mut ::VM) {
+    fn print_return_stack(vm: &mut ::VM) -> ::EvalResult {
         for (i, r) in vm.ret.iter().enumerate() {
             println!("{} - {}", i, vm.i_2_n[*r]);
         }
+        ::EvalResult::None
     }
 
-    fn quit(vm: &mut ::VM) {
-        vm.dead = true
+    fn quit(vm: &mut ::VM) -> ::EvalResult {
+        vm.dead = true;
+        ::EvalResult::None
     }
+
+    fn cm_set(vm: &mut ::VM) -> ::EvalResult {
+        match vm.stack.pop() {
+            Some(p) => {
+                vm.is_cm = if p == 0 { false } else { true };
+                ::EvalResult::None
+                },
+            None => ::EvalResult::StackUnderflow
+        }
+    }
+
+    fn cm_get(vm: &mut ::VM) -> ::EvalResult {
+        vm.stack.push(if vm.is_cm { 1 } else { 0 }) ;
+        ::EvalResult::None
+    }
+
+    fn get_addr(vm: &mut ::VM) -> ::EvalResult {
+        let tok = vm.read_token();
+        match vm.n_2_i.contains_key(&tok) {
+            true => { vm.stack.push(vm.n_2_i[&tok]); ::EvalResult::None },
+            false => ::EvalResult::WordNotFound(tok)
+        }
+    }
+
+    static CORE_ENTRIES : [NativeEntry; 10] = [
+        NativeEntry { name : "consume", is_macro: true, f: ::VM::consume_token  }, 
+        NativeEntry { name : "quit",    is_macro: true, f: quit                 }, 
+        NativeEntry { name : ".s",      is_macro: true, f: print_value_stack    }, 
+        NativeEntry { name : ".r",      is_macro: true, f: print_return_stack   }, 
+        NativeEntry { name : ":",       is_macro: true, f: define_word          }, 
+        NativeEntry { name : "!",       is_macro: true, f: define_immediate     }, 
+        NativeEntry { name : "ret",     is_macro: true, f: ret                  }, 
+        NativeEntry { name : "cm.set",  is_macro: true, f: cm_set               }, 
+        NativeEntry { name : "cm.get",  is_macro: true, f: cm_get               }, 
+        NativeEntry { name : "@",       is_macro: true, f: get_addr             }, 
+    ];
 
     pub fn register(vm: &mut ::VM) {
-        vm.register_native(String::from("consume"), true, ::VM::consume_token);
-        vm.register_native(String::from("quit"), true, quit);
-        vm.register_native(String::from("."), true, print_value_stack);
-        vm.register_native(String::from(".r"), true, print_return_stack);
-        vm.register_native(String::from("ret"), true, ret);
-        vm.register_native(String::from(":"), true, define_word);
-        vm.register_native(String::from("!"), true, define_immediate);
+        for ce in CORE_ENTRIES.iter() {
+            vm.register_native(ce.name.to_string(), ce.is_macro, ce.f);
+        }
     }
 }
 
@@ -197,6 +256,8 @@ impl VM {
             i_2_n   : Vec::new(),
             n_2_i   : HashMap::new(),
 
+            except  : EvalResult::None,
+
             is_cm   : false,             // is in compilation mode
             streams : Vec::new(),
             curr_sr : 0,
@@ -204,7 +265,7 @@ impl VM {
         }
     }
 
-    fn register_native(&mut self, name: String, is_macro: bool, f: fn(&mut VM)) {
+    fn register_native(&mut self, name: String, is_macro: bool, f: fn(&mut VM) -> EvalResult) {
         let idx = self.i_2_w.len();
         self.i_2_w.push(Word::Native { is_macro : is_macro, f : f});
         self.i_2_n.push(name.clone());
@@ -233,31 +294,31 @@ impl VM {
         }
     }
 
-    fn consume_token(&mut self) {
+    fn consume_token(&mut self) -> EvalResult {
         let tok = self.read_token();
         match (self.is_cm, VM::is_number(&tok), self.n_2_i.contains_key(&tok)) {
             // handle number
-            (true,  true, _    ) => self.code.push(OpCode::PushUSize(tok.parse::<usize>().unwrap())),
-            (false, true, _    ) => self.stack.push(tok.parse::<usize>().unwrap()),
+            (true,  true, _    ) => { self.code.push(OpCode::PushUSize(tok.parse::<usize>().unwrap())); EvalResult::None },
+            (false, true, _    ) => { self.stack.push(tok.parse::<usize>().unwrap()); EvalResult::None },
 
             // word
             (true,  _,    true ) => {
                 let wid = self.n_2_i[&tok];
                 match self.i_2_w[wid] {
-                    Word::Interp { is_macro: true, fip }    => { self.ret.push(core::WID_CONSUME); self.ip = fip },
+                    Word::Interp { is_macro: true, fip }    => { self.ret.push(core::WID_CONSUME); self.ip = fip; EvalResult::None },
                     Word::Native { is_macro: true, f }      => f(self),
-                    _                                       => { self.code.push(OpCode::Call(wid)) }, 
+                    _                                       => { self.code.push(OpCode::Call(wid)); EvalResult::None }, 
                 }
             },
             (false, _,    true ) => {
                 let wid = self.n_2_i[&tok];
                 match self.i_2_w[wid] {
-                    Word::Interp { is_macro: _, fip }    => { self.ret.push(core::WID_CONSUME); self.ip = fip },
+                    Word::Interp { is_macro: _, fip }    => { self.ret.push(core::WID_CONSUME); self.ip = fip; EvalResult::None },
                     Word::Native { is_macro: _, f }      => f(self),
                 }
             },
             // the word doesn't exist
-            (_,     _,    false) => println!("Word {} doesn't exist", tok),
+            (_,     _,    false) => EvalResult::WordNotFound(tok),
         }
     }
 
